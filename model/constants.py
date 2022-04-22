9bc26655fa8b3a6467bb67fd94e79c068db8a8cd
FallenChromium <fallenchromium@gmail.com>
2022-04-23 00:23:32 +0300

    typos :)

--- model/constants.py
+++ model/constants.py
@@ -1,6 +1,6 @@
 casts = ["Основной","Запасной", "N/A"]
 ranks = ["1-й юношеский", "2-й разряд", "3-й разряд", "Кмс", "Мастер спорта", "Нет"]
-fields = ["name", "cast", "position", "title", "sport", "rank"]
+fields = ["name", "cast", "position", "titles", "sport", "rank"]
 
 class IncorrectFileException(Exception):
     def __str__(self):
