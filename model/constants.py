d10081fc2f2bc1a62f04fe4c765d1c214bc23642
FallenChromium <fallenchromium@gmail.com>
2022-04-22 19:28:59 +0300

    Add constants

---
+++ model/constants.py
@@ -0,0 +1,7 @@
+casts = ["Основной","Запасной", "N/A"]
+ranks = ["1-й юношеский", "2-й разряд", "3-й разряд", "Кмс", "Мастер спорта", "Нет"]
+fields = ["name", "cast", "position", "title", "sport", "rank"]
+
+class IncorrectFileException(Exception):
+    def __str__(self):
+        print('Invalid file format!')
